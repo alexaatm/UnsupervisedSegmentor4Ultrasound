a6a11601f1bb17c2d95045b3bee3988793c1ffcc
Oleksandra Tmenova <oleksandra.tmenova@surgiceye.com>
2023-04-05 17:14:17 +0200

    modified init, optimizer and common step to account for optimizer config and for triplet base function

--- self-training/models/simclrTripletLightningModule.py
+++ self-training/models/simclrTripletLightningModule.py
@@ -5,15 +5,14 @@ from torch import nn
 from lightly.models.modules import SimCLRProjectionHead
 
 class SimCLRTriplet(pl.LightningModule):
-    def __init__(self, max_epochs=1, lr = 0.001):
+    def __init__(self, backbone, hidden_dim, max_epochs=1, lr = 0.001, optimizer="Adam"):
         super().__init__()
 
+        self.optimizer_choice=optimizer
         self.max_epochs = max_epochs
         self.lr=lr
-        resnet = torchvision.models.resnet18()
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-        hidden_dim = resnet.fc.in_features
-        self.projection_head = SimCLRProjectionHead(hidden_dim, hidden_dim, 128)
+        self.backbone=backbone
+        self.projection_head = SimCLRProjectionHead(hidden_dim, hidden_dim, 512)
         # https://pytorch.org/docs/stable/generated/torch.nn.TripletMarginLoss.html
         self.criterion = torch.nn.TripletMarginLoss()
 
@@ -30,8 +29,8 @@ class SimCLRTriplet(pl.LightningModule):
         return z_anchor, z_pos, z_neg
 
     def _common_step(self, batch, mode='train'):
-        (anchor, pos, neg) = batch
-        z_anchor, z_pos, z_neg = self.forward(anchor[0], pos[0], neg[0])
+        (anchor, _, _), (pos, _, _,), (neg, _, _) = batch #unpack the batch, lower dash_ stands for targets and fnames
+        z_anchor, z_pos, z_neg = self.forward(anchor, pos, neg)
         loss = self.criterion(z_anchor, z_pos, z_neg)
         self.log(f'{mode}_loss', loss)
         return loss
@@ -43,12 +42,16 @@ class SimCLRTriplet(pl.LightningModule):
         return self._common_step(batch, mode='val')
 
     def configure_optimizers(self):
-        # TODO: use Adam, as you are passing corresponding hydra config...
-        optim = torch.optim.SGD(
-            self.parameters(), lr=self.lr, momentum=0.9, weight_decay=5e-4
-        )
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optim, self.max_epochs
-        )
-
-        return [optim], [scheduler]
+        if self.optimizer_choice == "sdg":
+            optim = torch.optim.SGD(
+                self.parameters(), lr=self.lr, momentum=0.9, weight_decay=5e-4
+            )
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                optim, self.max_epochs
+            )
+            return [optim], [scheduler]
+        elif self.optimizer_choice == "Adam":
+            optim = torch.optim.Adam(self.parameters(), lr=self.lr)
+            return optim
+        else:
+            raise NotImplementedError()
